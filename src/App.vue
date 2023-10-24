cac9ae245caff0e0cd11ab706b5568543d47e0ca
humayonkabirr <humayonkobir8@gmail.com>
2023-10-24 18:55:36 +0600

    Initails commit

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div class="bg-[#2e2e2e] py-1 hidden md:block">
+    <ul class="flex mx-5 md:mx-24 text-white justify-end space-x-7 lg:space-x-16 text-[14px]">
+        <li><i class="fa-solid fa-money-bill px-2"></i>Cash On Delivery</li>
+        <li><i class="fa-solid fa-arrow-right-arrow-left px-2"></i> Fast Delivery And Exchange</li>
+        <li><i class="fa-regular fa-circle-check px-2"></i>100% Genuine</li>
+        <li>Language</li>
+        <li>Account</li>
+      </ul>
+  </div>
+  <div class="bg-[#2e2e2e] py-1 md:hidden">
+    <ul class="flex mx-5 md:mx-24 text-white justify-between space-x-7 lg:space-x-16 text-[14px]">
+        <li><i class="fa fa-phone"></i></li> 
+        <li><i class="fa fa-envelope"></i></li> 
+        <li><i class="fa fa-facebook-official"></i></li> 
+        <li><i class="fa fa-twitter"></i></li> 
+        <li><i class="fa fa-whatsapp"></i></li>  
+      </ul>
+  </div>
+  <header class="bg-[#ff0b34] py-2">
+    <nav class="mx-2 sm:mx-5 md:mx-24  flex items-center space-x-6">
+      <RouterLink to="/" class="w-[128px]">
+        <img class="w-full h-[52px]" src="https://nkrmart.com/public/storage/images/general_setting/HXnCqk8BOWmVIjZIT4xIkrJeJQf2tcKJw3oLKeDJ.png">
+      </RouterLink>
+      <ul class="flex text-white space-x-6">
+        <li class="hover:text-blue-300 cursor-pointer">Home</li>
+        <li class="hover:text-blue-300 cursor-pointer">About</li>
+        <li class="hover:text-blue-300 cursor-pointer">Contact</li>
+      </ul>
+      </nav>
+  </header>
+  
+
+  <RouterView />
+
+  <footer class="bg-white py-2 md:hidden sticky bottom-0 shadow-[0px_-4px_10px_#a4a4a4]">
+    
+    <nav class="mx-2 sm:mx-5 md:mx-24"> 
+      <ul class="w-full flex text-black justify-between px-4">
+        <li class="hover:text-blue-300 cursor-pointer text-center">
+          <router-link to="">
+            <i class="fa fa-home"></i>
+            <p class="uppercase text-[12px]">Home</p>
+          </router-link> 
+        </li>
+        <li class="hover:text-blue-300 cursor-pointer text-center">
+          <router-link to="">
+            <i class="fa fa-th-large"></i>
+            <p class="uppercase text-[12px]">Category</p>
+          </router-link> 
+        </li> 
+        <li class="hover:text-blue-300 cursor-pointer text-center">
+          <router-link to="">
+            <i class="fa fa-heart"></i>
+            <p class="uppercase text-[12px]">Wishlist</p>
+          </router-link> 
+        </li> 
+        <li class="hover:text-blue-300 cursor-pointer text-center">
+          <router-link to="">
+            <i class="fa fa-shopping-cart"></i>
+            <p class="uppercase text-[12px]">Bag</p>
+          </router-link> 
+        </li> 
+        <li class="hover:text-blue-300 cursor-pointer text-center">
+          <router-link to="">
+            <i class="fa fa-user-circle"></i>
+            <p class="uppercase text-[12px]">Me</p>
+          </router-link> 
+        </li> 
+      </ul>
+      </nav>
+  </footer>
+</template>
+
+<style scoped> 
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,270 @@
+<script setup>
+</script>
+
+<template>
+  <WelcomeItem>
+
+
+    <div class="m-2 sm:mx-5 md:mx-24">
+      <!-- Set up your HTML -->
+      <div class="owl-carousel" id="sliders">
+        <div class="" v-for="index in 5" :key="index">
+          <img src="https://nkrmart.com/public/storage/images/slider/XLBVNKFCwyr709clkQ9JftbSca2PEWY68KNJJMDj.jpg" alt="">
+        </div>
+      </div>
+
+      <!-- <div class="hidden grid-cols-0 lg:grid-cols-1 lg:block">
+        <img class="w-full" src="https://nkrmart.com/public/storage/images/banner/EvNKpy6uJb9iS1okoBwB8GqnnxCBz1VkqiEHyu4U.jpg" alt="">
+      </div> -->
+    </div>
+
+    <h1 class="mx-2 sm:mx-5 md:mx-24 mt-10 uppercase text-gray-700 text-xl lg:text-3xl font-extrabold text-center"
+      style="font-family: 'Dancing Script', cursive, sans-serif;">
+      Shop By Category
+    </h1>
+
+
+
+
+
+    <div class=" mx-auto w-full overflow-hidden relative">
+      <div class="w-full h-full absolute">
+        <div class="w-1/4 h-full absolute z-50 left-0"
+          style="background: linear-gradient(to right, #ffffff 0%, rgba(255, 255, 255, 0) 100%);"></div>
+        <div class="w-1/4 h-full absolute z-50 right-0"
+          style="background: linear-gradient(to left, #ffffff 0%, rgba(255, 255, 255, 0) 100%);"></div>
+      </div>
+
+      <div class="carousel-items flex items-center justify-center"
+        style="width: fit-content; animation: carouselAnim 10s infinite alternate linear;">
+
+        <div v-for="item in 12" :key="item"
+          class="carousel-focus flex items-center flex-col relative bg-white mx-5 my-10 p-2 border border-red-600 rounded-lg shadow-lg w-[200px]">
+          <img class="rounded-xl"
+            src="https://nkrmart.com/public/storage/images/subCategory/0RHRsQEQ4hHcFs8yU1bds6UwWdrOGm6Z5CbAADc0.jpg"
+            alt="Img">
+          <h1 class="mt-1">Shirt {{ item }}</h1>
+        </div>
+
+      </div>
+
+    </div>
+
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <div class="mx-2 sm:mx-5 md:mx-24 ">
+      <h1 class=" font-bold text-2xl">New Arrival Products</h1>
+
+      <div class="owl-slider">
+        <div id="carousel" class="owl-carousel">
+
+          <div class="item" v-for="index in 30" :key="index">
+            <div class="w-full max-w-sm bg-orange-50 border border-gray-200 rounded-lg shadow hover:shadow-xl">
+              <a href="#">
+                <img class="p-2 rounded-t-lg"
+                  src="https://nkrmart.com/public/storage/images/product_thumbnail_img/thumbnail_1697615262_1412.jpg"
+                  alt="product image" />
+              </a>
+              <div class="px-2 pb-2 w-full">
+                <a href="#">
+                  <h5 class="font-semibold tracking-tight text-sm text-gray-900 truncate">
+                    Apple Watch Series 7 GPS
+                  </h5>
+                </a>
+
+                <div class="flex items-center justify-between mb-2">
+                  <span class="text-xs text-gray-900">$599</span>
+                  <small class="font-bold text-red-600 line-through">$599</small>
+                </div>
+
+                <div class="flex w-full items-center justify-between">
+                  <a href="#"
+                    class="text-slate-900 w-full bg-orange-200 hover:bg-orange-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-md text-sm px-5 py-1.5 text-center ">
+                    Add to cart
+                  </a>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+    </div>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <div class="mx-2 sm:mx-5 md:mx-24 ">
+      <h1 class=" font-bold text-2xl">All Products </h1>
+
+
+
+      <div class="flex justify-center my-4 mt-6">
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-3">
+
+          <div v-for="index in 30" :key="index">
+            <div class="w-full max-w-sm bg-orange-50 border border-gray-200 rounded-lg shadow hover:shadow-xl">
+              <a href="#">
+                <img class="p-2 rounded-t-lg"
+                  src="https://nkrmart.com/public/storage/images/product_thumbnail_img/169529915412.%20Sky%20+%20Pest.jpg"
+                  alt="product image" />
+              </a>
+              <div class="px-2 pb-2 w-full">
+                <a href="#">
+                  <h5 class="font-semibold tracking-tight text-sm text-gray-900 truncate">
+                    Apple Watch Series 7 GPS
+                  </h5>
+                </a>
+
+                <div class="flex items-center justify-between mb-2">
+                  <span class="text-xs text-gray-900">$599</span>
+                  <small class="font-bold text-red-600 line-through">$599</small>
+                </div>
+
+                <div class="flex w-full items-center justify-between">
+                  <a href="#"
+                    class="text-slate-900 w-full bg-orange-200 hover:bg-orange-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-semibold rounded-md text-sm px-5 py-1.5 text-center ">
+                    Add to cart
+                  </a>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+
+
+    </div>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <div class="mx-2 sm:mx-5 md:mx-24 ">
+      <div class="flex justify-between py-16">
+        <div class="text-center">
+          <div class="service_container">
+            <div class="">
+              <div class=""><i
+                  class="fa fa-thumbs-up text-5xl text-white bg-[#FF0B34] p-4 rounded-[50%] border-8 border-[#c0c4c8]"></i>
+              </div>
+            </div>
+            <div class="service_text">
+              <h4>
+                High-quality Goods
+              </h4>
+              <p>
+                Enjoy top quality items for less
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="text-center">
+          <div class="service_container">
+            <div class="service_outer">
+              <div class="service_inner"><i
+                  class="fa fa-headphones text-5xl text-white bg-[#FF0B34] p-4 rounded-[50%] border-8 border-[#c0c4c8]"></i>
+              </div>
+            </div>
+            <h4>
+              24/7 Live Chat
+            </h4>
+            <p>
+              Get instant assistance whenever you need it
+            </p>
+          </div>
+        </div>
+        <div class="text-center">
+          <div class="service_container">
+            <div class="service_outer">
+              <div class="service_inner"><i
+                  class="fa fa-truck text-5xl text-white bg-[#FF0B34] p-4 rounded-[50%] border-8 border-[#c0c4c8]"></i>
+              </div>
+            </div>
+            <h4>
+              Express Shipping
+            </h4>
+            <p>
+              Fast &amp; reliable delivery options
+            </p>
+          </div>
+        </div>
+        <div class="text-center">
+          <div class="service_container">
+            <div class="service_outer">
+              <div class="service_inner"><i
+                  class="fa fa-credit-card text-5xl text-white bg-[#FF0B34] p-4 rounded-[50%] border-8 border-[#c0c4c8]"></i>
+              </div>
+            </div>
+            <h4>
+              Secure Payment
+            </h4>
+            <p>
+              Multiple safe payment methods
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </WelcomeItem>
+
+  <WelcomeItem>
+    <footer class="bg-[#ff0b34] py-8">
+      <div class="mx-2 sm:mx-5 md:mx-24">
+        <div class=" grid grid-cols-3">
+            <div>
+              
+            </div>
+        </div>
+      </div>
+
+
+      <div class="footer_info_row">
+        <div class="end_footer">
+          <p class="f_info_left">
+            Copyright © nkrmart.com 2022 All rights reserved
+          </p>
+          <p class="f_info_right"><a href="https://mohasagorit.solutions" target="_blank"
+              style="color: rgb(255, 255, 255);">Developed by MIT</a></p>
+        </div>
+      </div>
+    </footer>
+  </WelcomeItem>
+</template>
+
+<style>
+@keyframes carouselAnim {
+  from {
+    transform: translate(0, 0);
+  }
+
+  to {
+    transform: translate(calc(-100% + (6*300px)));
+  }
+}
+
+@media only screen and (max-width: 768px) {
+  .container .carousel-items {
+    animation: carouselAnim 60s infinite alternate linear;
+  }
+
+  @keyframes carouselAnim {
+    from {
+      transform: translate(0, 0);
+    }
+
+    to {
+      transform: translate(calc(-100% + (5*300px)));
+    }
+  }
+}
+
+.carousel-focus:hover {
+  transition: all 0.8s;
+  transform: scale(1.1);
+}
+</style>
